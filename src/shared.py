5b63564db84900dcc5c73e36aa9daa3f68e1f9c0
mitchell <123deletme@gmail.com>
2020-04-30 03:37:56 -0400

    mend
    seperated into class files

---
+++ src/shared.py
@@ -0,0 +1,23 @@
+
+#blackBoardBaseURL = "lms.curtin.edu.au"
+blackBoardBaseURL = 'uoit.blackboard.com'
+
+valid_chars = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-'
+
+#sanitizes the filenames for windows (and hopefully other OS' too!)
+def sanitize(filename):
+    filename = filename.strip()
+    filename = filename.replace(":", "--").replace("/", "-")
+
+    filename = ''.join(c for c in filename if c in valid_chars)
+    try:
+        while ((filename[len(filename)-1] == ' ') or (filename[len(filename)-1] == '.')):
+            filename = filename[:-1]
+        while (filename.count("  ") != 0):
+            filename = filename.replace("  ", " ")
+    except IndexError:
+        if len(filename) < 1:
+            return 'file_' + filename
+        else:
+            return filename.strip()
+    return filename.strip()
